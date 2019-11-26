fbe63d9f969d6c3e681cbf848db97e507f614391
sunyangj <375626456@qq.com>
2019-11-26 18:21:23 +0800

    初始化

---
+++ src/avicit/bpmevent/WorldCustom.java
@@ -0,0 +1,32 @@
+package avicit.bpmevent;
+
+import avicit.platform6.bpm.api.activity.ActivityExecution;
+import avicit.platform6.bpm.api.activity.ExternalActivityBehaviour;
+
+import java.util.Map;
+
+public class WorldCustom implements ExternalActivityBehaviour {
+
+	@Override
+	public void execute(ActivityExecution paramActivityExecution) throws Exception {
+		// 流程执行Id
+		String executionId = paramActivityExecution.getId().toString();
+		// 流程实例Id
+		String processInstanceId =paramActivityExecution.getProcessInstanceId().toString();
+		// 业务表单Id
+		String formId1 = (String) paramActivityExecution.getVariable("id");
+		// 执行业务逻辑
+		System.out.println("Custom2节点输出：World");
+		// 驱动流程继续流转
+		paramActivityExecution.takeDefaultTransition();
+		// 流程暂停，等待驱动信号
+		// execution.waitForSignal();
+	}
+
+	@Override
+	public void signal(ActivityExecution paramActivityExecution, String paramString, Map<String, ?> paramMap)
+			throws Exception {
+		paramActivityExecution.take(paramString);
+	}
+
+}
